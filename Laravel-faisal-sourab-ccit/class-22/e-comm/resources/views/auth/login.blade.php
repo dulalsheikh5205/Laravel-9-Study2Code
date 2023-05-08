02b72b8568e06fec6203a8b4558b013a18981590
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-08 10:06:05 +0600

    add css/js {{ asset('')}} and naming routing link {{ route('')}} and add avatar pic https://api.dicebear.com/api/initials/dulal.svg and src='https://api.dicebear.com/api/initials/{{ auth()->user()->name }}.svg'

--- Laravel-faisal-sourab-ccit/class-22/e-comm/resources/views/auth/login.blade.php
+++ Laravel-faisal-sourab-ccit/class-22/e-comm/resources/views/auth/login.blade.php
@@ -37,26 +37,35 @@
                 </div>
                 <!-- END: Login Info -->
                 <!-- BEGIN: Login Form -->
-                <div class="g-col-2 g-col-xl-1 h-screen h-xl-auto d-flex py-5 py-xl-0 my-10 my-xl-0">
+                <form action="{{ route('login') }}" method="POST" class="g-col-2 g-col-xl-1 h-screen h-xl-auto d-flex py-5 py-xl-0 my-10 my-xl-0">
+                
+                    @csrf
+
                     <div class="my-auto mx-auto ms-xl-20 bg-white dark-bg-dark-1 bg-xl-transparent px-5 px-sm-8 py-8 p-xl-0 rounded-2 shadow-md shadow-xl-none w-full w-sm-3/4 w-lg-2/4 w-xl-auto">
                         <h2 class="intro-x fw-bold fs-2xl fs-xl-3xl text-center text-xl-start">
                             Sign In
                         </h2>
                         <div class="intro-x mt-2 text-gray-500 d-xl-none text-center">A few more clicks to sign in to your account. Manage all your e-commerce accounts in one place</div>
                         <div class="intro-x mt-8">
-                            <input type="text" class="intro-x login__input form-control py-3 px-4 border-gray-300 d-block" placeholder="Email">
-                            <input type="password" class="intro-x login__input form-control py-3 px-4 border-gray-300 d-block mt-4" placeholder="Password">
+                            <input type="text" class="intro-x login__input form-control py-3 px-4 border-gray-300 d-block" placeholder="Email" name="email">
+                            <input type="password" class="intro-x login__input form-control py-3 px-4 border-gray-300 d-block mt-4" placeholder="Password" name="password">
+
+                            @error('email')
+                            <span class="alert alert-danger my-4 d-block">{{ $message }}</span>
+                           @enderror
+                        
+
                         </div>
                         <div class="intro-x d-flex text-gray-700 dark-text-gray-600 fs-xs fs-sm-sm mt-4">
                             <div class="d-flex align-items-center me-auto">
-                                <input id="remember-me" type="checkbox" class="form-check-input border me-2">
+                                <input id="remember-me" type="checkbox" class="form-check-input border me-2" name="remember" {{ old('remember') ? 'checked' : '' }}>
                                 <label class="cursor-pointer select-none" for="remember-me">Remember me</label>
                             </div>
                             <a href="login-light-login.html">Forgot Password?</a> 
                         </div>
                         <div class="intro-x mt-5 mt-xl-8 text-center text-xl-start">
                             <button class="btn btn-primary py-3 px-4 w-full w-xl-32 me-xl-3 align-top">Login</button>
-                            <button class="btn btn-outline-secondary py-3 px-4 w-full w-xl-32 mt-3 mt-xl-0 align-top">Sign up</button>
+                            <a href="{{ route('register') }}" class="btn btn-outline-secondary py-3 px-4 w-full w-xl-32 mt-3 mt-xl-0 align-top">Sign up</a>
                         </div>
                         <div class="intro-x mt-10 mt-xl-24 text-gray-700 dark-text-gray-600 text-center text-xl-start">
                             By signin up, you agree to our 
@@ -64,7 +73,8 @@
                             <a class="text-theme-1 dark-text-theme-10" href="login-light-login.html">Terms and Conditions</a> & <a class="text-theme-1 dark-text-theme-10" href="login-light-login.html">Privacy Policy</a> 
                         </div>
                     </div>
-                </div>
+                
+            </form>
                 <!-- END: Login Form -->
             </div>
         </div>
