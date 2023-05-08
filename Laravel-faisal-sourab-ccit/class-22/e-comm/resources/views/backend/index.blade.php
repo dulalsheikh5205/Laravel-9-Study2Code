e5bb4ff9fde7fe05c5e4b79c054fb350a8259d04
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-08 10:29:55 +0600

    add user and user logout section

--- Laravel-faisal-sourab-ccit/class-22/e-comm/resources/views/backend/index.blade.php
+++ Laravel-faisal-sourab-ccit/class-22/e-comm/resources/views/backend/index.blade.php
@@ -1334,7 +1334,7 @@ License: You must have a valid license purchased only from themeforest(the above
                         <div class="dropdown-menu w-56">
                             <ul class="dropdown-content bg-theme-26 dark-bg-dark-6 text-white">
                                 <li class="p-2">
-                                    <div class="fw-medium text-white">Robert De Niro</div>
+                                    <div class="fw-medium text-white">{{ str()->headline(auth()->user()->name) }}</div>
                                     <div class="fs-xs text-theme-28 mt-0.5 dark-text-gray-600">DevOps Engineer</div>
                                 </li>
                                 <li>
@@ -1343,20 +1343,25 @@ License: You must have a valid license purchased only from themeforest(the above
                                 <li>
                                     <a href="index.html" class="dropdown-item text-white bg-theme-1-hover dark-bg-dark-3-hover"> <i data-feather="user" class="w-4 h-4 me-2"></i> Profile </a>
                                 </li>
-                                <li>
-                                    <a href="index.html" class="dropdown-item text-white bg-theme-1-hover dark-bg-dark-3-hover"> <i data-feather="edit" class="w-4 h-4 me-2"></i> Add Account </a>
-                                </li>
+                               
                                 <li>
                                     <a href="index.html" class="dropdown-item text-white bg-theme-1-hover dark-bg-dark-3-hover"> <i data-feather="lock" class="w-4 h-4 me-2"></i> Reset Password </a>
                                 </li>
-                                <li>
-                                    <a href="index.html" class="dropdown-item text-white bg-theme-1-hover dark-bg-dark-3-hover"> <i data-feather="help-circle" class="w-4 h-4 me-2"></i> Help </a>
-                                </li>
+                              
                                 <li>
                                     <hr class="dropdown-divider border-theme-27 dark-border-dark-3">
                                 </li>
                                 <li>
-                                    <a href="index.html" class="dropdown-item text-white bg-theme-1-hover dark-bg-dark-3-hover"> <i data-feather="toggle-right" class="w-4 h-4 me-2"></i> Logout </a>
+                                    <a href="{{ route('logout') }}" class="dropdown-item text-white bg-theme-1-hover dark-bg-dark-3-hover" onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                      <i data-feather="toggle-right" class="w-4 h-4 me-2"></i> Logout </a>
+
+                                  
+
+                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                     @csrf
+                                 </form>
+
                                 </li>
                             </ul>
                         </div>
