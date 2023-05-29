81f345bd4288ce071d96d39fb4ad5339a2d3b0ca
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-29 22:40:46 +0600

    ostad module-15 assignment

--- Laravel-9-std/Module-15/assignment-module-15/Assignment-15/routes/api.php
+++ Laravel-9-std/Module-15/assignment-module-15/Assignment-15/routes/api.php
@@ -1,42 +1,19 @@
 <?php
 
-use App\Http\Controllers\AssignmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request){
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-// Question 1
-Route::post('/userinfo',[AssignmentController::class,'UserInfo']);
-
-// Question 2
-Route::post('/useragent',[AssignmentController::class,'UserAgent']);
-
-// Question 3
-Route::get('/query',[AssignmentController::class,'ApiEndpoint']);
-
-// Question 4
-Route::get('/jsonresponse',[AssignmentController::class,'JsonResponse']);
-
-// Question 5
-Route::post('/upload',[AssignmentController::class,'FileUploads']);
-
-// Question 6
-Route::post('/setcookie',[AssignmentController::class,'SetCookie']);
-
-// Question 7
-Route::post('/submit',function(Request $request){
-    $email = $request->input('email');
-    if($email){
-        return array(
-            "success"=> true,
-            "message"=> "Form submitted successfully."
-        );
-    }else{
-        return "Email must not be empty!";
-    }
-});
-// Assignment complete(module-14)
